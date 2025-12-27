b5ff9e879c2b53674fa6533e4b9160bf289df661
TrungThanh-Cao <dev.thanhcao@gmail.com>
2025-12-27 21:46:52 +0700

    UI Posts

--- resources/Frontend/js/Components/Card/CardPostRow.vue
+++ resources/Frontend/js/Components/Card/CardPostRow.vue
@@ -1,53 +1,37 @@
 <template>
-    <div v-if="item.slug" class="grid grid-cols-12 bg-white group">
+    <div v-if="item.slug" class="flex items-start md:gap-2.5 gap-1.5 xl:gap-[14px] group">
         <Link
             :href="
                 route('posts.show', {
                     slug: item.slug,
                 })
             "
-            class="col-span-full md:col-span-7 aspect-w-8 aspect-h-5 relative overflow-hidden block"
+            class="max-w-[171px] w-full relative overflow-hidden rounded-2xl block"
         >
-            <JPicture
-                :src="item.image.url || '/cover.jpg'"
-                :alt="item.image.alt || item.title || 'image card post'"
-                wrapperClass="picture-cover"
-                class="w-full h-full object-cover lg:group-hover:scale-105 duration-300 ease-in-out"
-            />
+            <div class="aspect-w-3 aspect-h-2">
+                <JPicture
+                    :src="item.image.url || '/cover.jpg'"
+                    :alt="item.image.alt || item.title || 'image card post'"
+                    wrapperClass="picture-cover"
+                    class="w-full h-full object-cover lg:group-hover:scale-105 duration-300 ease-in-out"
+                />
+            </div>
         </Link>
 
-        <div class="col-span-full md:col-span-5 p-3 md:p-4 xl:p-6 flex items-center justify-center">
-            <div class="space-y-3 md:space-y-4 xl:space-y-6">
-                <div
-                    class="body-2 py-0.5 px-3 bg-primary-100 text-primary-900 rounded-full w-max flex items-center justify-center"
-                >
-                    <span>{{ item.category.title }}</span>
-                </div>
-                <div class="space-y-2">
-                    <Link
-                        :href="
-                            route('posts.show', {
-                                slug: item.slug,
-                            })
-                        "
-                        class="headline-1 text-gray-900 !font-sans lg:group-hover:text-primary duration-300 ease-in-out"
-                    >
-                        {{ item.title }}
-                    </Link>
-                    <p class="body-1 text-gray-700 line-clamp-3">
-                        {{ item.description }}
-                    </p>
-                </div>
-                <Link
-                    :href="
-                        route('posts.show', {
-                            slug: item.slug,
-                        })
-                    "
-                    class="btn four"
-                    >Xem thêm</Link
-                >
-            </div>
+        <div class="flex-1 w-full md:space-y-2.5 space-y-1.5 xl:space-y-[14px]">
+            <Link
+                :href="
+                    route('posts.show', {
+                        slug: item.slug,
+                    })
+                "
+                class="title-2 line-clamp-2 font-bold text-gray-900 lg:group-hover:text-primary duration-300 ease-in-out"
+            >
+                {{ item.title }}
+            </Link>
+            <p class="body-1 text-gray-700 line-clamp-2">
+                {{ item.description }}
+            </p>
         </div>
     </div>
 </template>
--- resources/Frontend/js/Pages/Posts/Index.vue
+++ resources/Frontend/js/Pages/Posts/Index.vue
@@ -1,63 +1,192 @@
 <template>
-    <main class="bg-primary-25">
-        <BannerImage :banner="bannerBlog" classBanner="h-[320px]" />
-        <section class="py-12">
-            <div class="container md:space-y-6 space-y-4 xl:space-y-8">
-                <div class="md:flex items-center justify-end">
-                    <div class="flex items-center gap-3">
-                        <CustomSelect title="Danh mục" :options="categories" filterKey="categories" type="post" />
-                        <CustomSelect
-                            title="Sắp xếp"
-                            :options="[
-                                { title: 'Mới nhất', value: 'desc' },
-                                { title: 'Cũ nhất', value: 'asc' },
-                                { title: 'A → Z', value: 'az' },
-                                { title: 'Z → A', value: 'za' },
-                            ]"
-                            filterKey="sort"
-                            type="post"
-                        />
-                    </div>
-                </div>
-                <div v-if="posts.data && posts.data.length">
+    <main>
+        <BannerImage :banner="banner" classBanner="h-[320px]" />
+        <section class="py-20">
+            <div class="container md:space-y-6 space-y-4 xl:space-y-12">
                     <div
-                        class="grid grid-cols-2 md:gap-6 gap-3 xl:gap-8 border-b border-gray-200 md:pb-6 pb-4 xl:pb-8 notranslate"
+                        class="grid md:grid-cols-2 md:gap-6 gap-4 xl:gap-12"
                     >
-                        <CardCardPostRow class="col-span-full" :item="posts.data[0]" />
                         <CardCardPost
-                            v-for="(itemPost, indexPost) in posts.data.slice(1)"
-                            :key="indexPost"
-                            :item="itemPost"
+                            :item="postsHot.data[0]"
                         />
+                        <div class="flex flex-col md:gap-6 gap-4 xl:gap-12">
+                            <CardCardPostRow v-for="(itemPostHot, indexPostHot) in postsHot.data.slice(1,4)" :key="indexPostHot" :item="itemPostHot" />
+                        </div>
                     </div>
-                    <Pagination :links="posts.links" class="pt-3 pb-4" />
-                </div>
+                    <div class="grid grid-cols-2 lg:grid-cols-3 md:gap-6 gap-4 xl:gap-12">
+                        <CardCardPost v-for="(itemPost, indexPost) in posts.data" :key="indexPost" :item="itemPost" />
+                    </div>
+                    <Pagination :links="posts.links" />
             </div>
         </section>
     </main>
 </template>
 <script>
-import JamBreadcrumb from '@/Components/Jam/Breadcrumb.vue'
 import Pagination from '@/Components/Paginate.vue'
-import LogoSymbol from '@/Components/Icons/LogoSymbol.vue'
 
 export default {
-    components: { JamBreadcrumb, Pagination, LogoSymbol },
+    components: { Pagination },
 
     props: ['categories', 'posts', 'banner'],
     data() {
         return {
-            bannerBlog: {
-                title: this.tt('Góc Nhìn Từ Sip N Source'),
-                description: this.tt('Chia sẻ kiến thức, kinh nghiệm và cảm hứng từ những chiến dịch'),
-                image: '/assets/images/posts/bg-banner.webp',
-                breadcrumbs: [
-                    {
-                        title: this.tt('Bài viết'),
-                    },
-                ],
+            banner: {
+                title: this.tt('News'),
+                image: '/assets/images/posts/banner.jpg',
             },
-        }
+            postsHot: {
+                data: [
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                ]
+            }
+,
+            posts: {
+                data: [
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                    {
+                        image: {
+                            url: "/assets/images/posts/banner.jpg",
+                            alt: "image demo"
+                        },
+                        title: "VietNam Farmer Expands Export to Japan",
+                        slug: "demo",
+                        description: "We are proud to announce our first shipment of Cavendish bananas to Japan, a market known for its strict quality standards. This milestone reflects our commitment to global growth and premium quality."
+                    },
+                ]
+            }        }
     },
 }
 </script>
