af590364a06dfadaa34fa4c2348467245974b338
TrungThanh-Cao <dev.thanhcao@gmail.com>
2026-02-05 20:36:20 +0700

    Push code

--- resources/Frontend/js/Pages/Posts/Index.vue
+++ resources/Frontend/js/Pages/Posts/Index.vue
@@ -3,20 +3,17 @@
         <BannerImage :banner="banner" classBanner="h-[320px]" />
         <section class="py-20">
             <div class="container md:space-y-6 space-y-4 xl:space-y-12">
-                    <div
-                        class="grid md:grid-cols-2 md:gap-6 gap-4 xl:gap-12"
-                    >
-                        <CardCardPost
-                            :item="top_posts[0]"
-                        />
-                        <div class="flex flex-col md:gap-6 gap-4 xl:gap-12">
-                            <CardCardPostRow v-for="(itemPostHot, indexPostHot) in top_posts.slice(1,4)" :key="indexPostHot" :item="itemPostHot" />
-                        </div>
+                <div class="grid md:grid-cols-2 md:gap-6 gap-4 xl:gap-12">
+                    <CardCardPost :item="top_posts[0]" />
+                    <div class="flex flex-col md:gap-6 gap-4 xl:gap-12">
+                        <CardCardPostRow v-for="(itemPostHot, indexPostHot) in top_posts.slice(1, 4)" :key="indexPostHot"
+                            :item="itemPostHot" />
                     </div>
-                    <div class="grid grid-cols-2 lg:grid-cols-3 md:gap-6 gap-4 xl:gap-12">
-                        <CardCardPost v-for="(itemPost, indexPost) in posts.data" :key="indexPost" :item="itemPost" />
-                    </div>
-                    <Pagination :links="posts.links" />
+                </div>
+                <div class="grid grid-cols-2 lg:grid-cols-3 md:gap-6 gap-4 xl:gap-12">
+                    <CardCardPost v-for="(itemPost, indexPost) in posts.data" :key="indexPost" :item="itemPost" />
+                </div>
+                <Pagination :links="posts.links" />
             </div>
         </section>
     </main>
@@ -31,10 +28,10 @@ export default {
     data() {
         return {
             banner: {
-                title: this.tt('News'),
+                title: this.tt('Tin tức'),
                 image: '/assets/images/posts/banner.jpg',
             },
         }
-    }, 
+    },
 }
 </script>
